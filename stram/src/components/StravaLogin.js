a38ac4463cc1bfd2e03ad1b61e3953556c50257f
Jan <Ossenblokj@gmail.com>
2025-02-16 13:31:44 +0100

    co

--- stram/src/components/StravaLogin.js
+++ stram/src/components/StravaLogin.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const StravaLogin = () => {
 	const handleLogin = () => {
-		const clientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
-		console.log('Client ID:', process.env.REACT_APP_STRAVA_CLIENT_ID);
+		const clientId = window.STRAVA_CONFIG.CLIENT_ID
+		console.log('Client ID:', window.STRAVA_CONFIG.CLIENT_ID);
 		const redirectUri = encodeURIComponent('https://R0820395.github.io/stram/callback');
 		const scope = 'activity:read_all';
 
